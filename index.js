9d83ccfb5f2fe7d200913b932d43c6a8a6ab4e0d
vikashkumar6207 <vktd828403@gmail.com>
2025-11-16 16:36:01 +0530

    restfullApi project 01

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+
+const users = require("./MOCK_DATA.json");
+
+const app = express();
+const PORT = 8000;
+
+// Routs
+app.get("/users" , (req, res) => {
+    const html = `
+    <ul>
+    ${users.map(user => `<li>${user.first_name}</li>`).join("")}
+    </ul>
+    `;
+    res.send(html);
+ });
+
+ app.get("/api/users" , (req, res) => {
+    return res.json(users);
+ });
+
+//    app.get("/api/users/:id", (req, res) => {
+//       const id = Number(req.params.id);
+//       const user = users.find((user) => user.id === id);
+//     return res.json(user);
+//  });
+
+
+ app.post('/api/users', (req, res) => {
+   //  TODO : Create new user
+   return res.json({status: "pending"});
+ });
+
+// app.patch('/api/users/:id', (req, res) => {
+//    //  TODO : Edit the user with id
+//    return res.json({status: "pending"});
+//  });
+
+//  app.delete('/api/users/:id', (req, res) => {
+//     //  TODO : Delete the user with id
+//    return res.json({status: "pending"});
+//  });
+
+app.route("/api/users/:id").get((req, res) => {
+      const id = Number(req.params.id);
+      const user = users.find((user) => user.id === id);
+    return res.json(user);
+ }).patch((req, res) => {
+   //Edit the user with id
+   return res.json({status: "Pending"});
+ })
+ .delete((req, res)=>{
+   //Delete the user with id
+   return res.json({status: "Pending"});
+ });
+app.listen(PORT, () => console.log(`Server started at PORT : ${PORT}`));
